1315ebf595b96df5d6e30d2fb396d0b8bdb55d2a
Gu Junhua <jhgu@nao.cas.cn>
2019-02-07 21:09:04 +0800

    modified:   src/integration/adaptive_trapezoid.rs 	modified:   src/main.rs

--- src/integration/adaptive_trapezoid.rs
+++ src/integration/adaptive_trapezoid.rs
@@ -10,6 +10,20 @@ struct Point<T> {
     f: T,
 }
 
+fn neumaier_sum<T>(x: T, mut sum: T, mut comp: T) -> (T, T)
+where
+    T: Float + Debug,
+{
+    let t = sum + x;
+    if sum.abs() >= x.abs() {
+        comp = comp + ((sum - t) + x);
+    } else {
+        comp = comp + ((x - t) + sum);
+    }
+    sum = t;
+    (sum, comp)
+}
+
 pub fn integrate<T>(func: &dyn Fn(T) -> T, eps: T, init_ticks: &[T]) -> T
 where
     T: Float + Debug,
@@ -22,55 +36,8 @@ where
     let half = T::one() / two;
     let quarter = half / two;
     let full_width = *init_ticks.last().unwrap() - *init_ticks.first().unwrap();
-    let mut areas = Vec::<T>::new();
-    let mut points: Vec<_> = init_ticks
-        .iter()
-        .map(|&x| Point::<T> { x, f: func(x) })
-        .collect();
-    //let mut right=*points.last().unwrap();
-    let mut right;
-    let eps = eps * four / full_width;
-    while points.len() > 1 {
-        right = points.pop().unwrap();
-        let left = *points.last().unwrap();
-        let mid = (left.x + right.x) * half;
-        let fmid = func(mid);
-        if (left.f + right.f - fmid * two).abs() <= eps {
-            areas.push((left.f + right.f + fmid * two) * (right.x - left.x) * quarter);
-        //points.pop();
-        //right=left;
-        } else {
-            //points.pop();
-            points.push(Point::<T> { x: mid, f: fmid });
-            points.push(right);
-        }
-    }
-    (&mut areas).sort_by(|a, b| {
-        //(&mut areas).sort_unstable_by(|a, b| {
-        if a.abs() < b.abs() {
-            std::cmp::Ordering::Less
-        } else if a.abs() > b.abs() {
-            std::cmp::Ordering::Greater
-        } else {
-            std::cmp::Ordering::Equal
-        }
-    });
-    areas.into_iter().fold(T::zero(), |a, b| a + b)
-}
-
-pub fn integrate_nosort<T>(func: &dyn Fn(T) -> T, eps: T, init_ticks: &[T]) -> T
-where
-    T: Float + Debug,
-{
-    if init_ticks.len() <= 1 {
-        return T::zero();
-    }
-    let two = T::one() + T::one();
-    let four = two + two;
-    let half = T::one() / two;
-    let quarter = half / two;
-    let full_width = *init_ticks.last().unwrap() - *init_ticks.first().unwrap();
-    let mut area = T::zero();
+    let mut total_area = T::zero();
+    let mut comp = T::zero();
     let mut points: Vec<_> = init_ticks
         .iter()
         .map(|&x| Point::<T> { x, f: func(x) })
@@ -85,7 +52,10 @@ where
         let mid = (left.x + right.x) * half;
         let fmid = func(mid);
         if (left.f + right.f - fmid * two).abs() <= eps {
-            area = area + (left.f + right.f + fmid * two) * (right.x - left.x) * quarter;
+            let area = (left.f + right.f + fmid * two) * (right.x - left.x) * quarter;
+            let (s, c) = neumaier_sum(area, total_area, comp);
+            total_area = s;
+            comp = c;
         //points.pop();
         //right=left;
         } else {
@@ -94,5 +64,5 @@ where
             points.push(right);
         }
     }
-    area
+    total_area + comp
 }
