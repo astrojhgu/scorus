03458049664c22a240d12392a0006e6250b7f605
Gu Junhua <jhgu@nao.cas.cn>
2020-01-25 21:05:40 +0800

    new file:   src/mcmc/graph/node_tag.rs 	new file:   src/mcmc/graph/utils.rs

---
+++ src/mcmc/graph/node_tag.rs
@@ -0,0 +1,22 @@
+use std::hash::Hash;
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+pub enum NodeTag<T>
+where
+    T: Clone + Eq + Hash + std::fmt::Debug,
+{
+    Scalar(T),
+    Vector(T, usize),
+}
+
+impl<T> std::fmt::Display for NodeTag<T>
+where
+    T: Clone + Eq + Hash + std::fmt::Display + std::fmt::Debug,
+{
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+        match self {
+            &NodeTag::Scalar(ref x) => write!(f, "{}", x)?,
+            &NodeTag::Vector(ref x, _) => write!(f, "{}[..]", x)?,
+        };
+        Ok(())
+    }
+}
---
+++ src/mcmc/graph/utils.rs
@@ -0,0 +1,60 @@
+use super::graph::Graph;
+use super::node::NodeContent::{DeterministicNode, StochasticNode};
+use num_traits::Float;
+use rand::distributions::uniform::SampleUniform;
+use rand::distributions::StandardNormal;
+use rand::distributions::{Distribution, Standard};
+use std::collections::HashMap;
+use std::fmt::Debug;
+use std::fmt::Display;
+impl<K, T> Graph<K, T>
+where
+    K: std::hash::Hash + Eq + Clone + Display + Debug,
+    T: Float + Sync + SampleUniform + Send + Display + Debug,
+    Standard: Distribution<T>,
+{
+    pub fn dump_topology(&self, w: &mut std::io::Write) {
+        let topology = self.topology();
+        let mut edges = std::collections::HashMap::<String, Vec<String>>::new();
+        let mut nodes = std::collections::HashSet::<String>::new();
+        for (k, v) in topology {
+            let kname = format!("\"{}\"", k);
+            let plist: Vec<_> = v.into_iter().map(|t| format!("\"{}\"", t.0)).collect();
+
+            let mut node_decl = kname.clone();
+
+            match self.get_node(&k).content {
+                DeterministicNode { .. } => {
+                    node_decl += "[shape=diamond, style=rounded]";
+                }
+                StochasticNode {
+                    ref is_observed, ..
+                } => {
+                    if is_observed.iter().any(|&x| x) {
+                        node_decl += "[shape=box]";
+                    } else {
+                        node_decl += "[shape=circle]";
+                    }
+                }
+            }
+
+            nodes.insert(node_decl);
+            edges.insert(kname, plist);
+        }
+
+        writeln!(w, "digraph G");
+        writeln!(w, "{{");
+        for n in nodes {
+            writeln!(w, "{}", n);
+        }
+        for (k, v) in edges {
+            if !v.is_empty() {
+                for j in v {
+                    writeln!(w, "{} -> {}", j, k);
+                }
+            }
+        }
+
+        writeln!(w, "}}");
+    }
+}
