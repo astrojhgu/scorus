476d5d63262ce970bac12cbd572e08f70d7354ab
Gu Junhua <jhgu@nao.cas.cn>
2020-01-29 19:42:36 +0800

    modified:   src/mcmc/ensemble_sample.rs

--- src/mcmc/ensemble_sample.rs
+++ src/mcmc/ensemble_sample.rs
@@ -11,10 +11,12 @@ use rand::distributions::uniform::SampleUniform;
 use rand::distributions::Distribution;
 use rand::distributions::Standard;
 use rand::Rng;
+use rand::seq::SliceRandom;
 use std::marker::{Send, Sync};
 use std::ops::IndexMut;
 use std::ops::{Add, Mul, Sub};
 use std::sync::Mutex;
+
 //use std::sync::Arc;
 
 use super::mcmc_errors::McmcErr;
@@ -24,75 +26,26 @@ use crate::linear_space::LinearSpace;
 use crate::utils::HasLen;
 use crate::utils::InitFromLen;
 
-pub fn create_sampler<'a, T, U, V, W, X, F>(
-    flogprob: F,
-    mut ensemble_logprob: (W, X),
-    mut rng: U,
-    a: T,
-    nthread: usize,
-) -> Box<dyn 'a + FnMut(&mut dyn FnMut(&Result<(W, X), McmcErr>)) -> ()>
-where
-    T: 'static
-        + Float
-        + NumCast
-        + std::cmp::PartialOrd
-        + SampleUniform
-        + Sync
-        + Send
-        + std::fmt::Display,
+pub fn propose_move<T, V>(p1: &V, p2: &V, z: T)->V
+where 
+    T: Float + NumCast + std::cmp::PartialOrd + SampleUniform + Sync + Send + std::fmt::Display,
     Standard: Distribution<T>,
-    U: 'static + Rng,
     V: Clone + LinearSpace<T> + Sync + Send + Sized,
     for<'b> &'b V: Add<Output = V>,
     for<'b> &'b V: Sub<Output = V>,
     for<'b> &'b V: Mul<T, Output = V>,
-    W: 'static + Clone + IndexMut<usize, Output = V> + HasLen + Sync + Send + Sized,
-    X: 'static + Clone + IndexMut<usize, Output = T> + HasLen + Sync + InitFromLen + Send + Sized,
-    F: 'a + Fn(&V) -> T + Send + Sync + Sized,
 {
-    Box::new(move |handler: &mut dyn FnMut(&Result<(W, X), McmcErr>)| {
-        let result = sample(&flogprob, &ensemble_logprob, &mut rng, a, nthread);
-        handler(&result);
-        if let Ok(x) = result {
-            ensemble_logprob = x
-        }
-    })
+    scale_vec(p1, p2, z)
 }
 
-pub fn create_sampler_st<'a, T, U, V, W, X, F>(
-    flogprob: F,
-    mut ensemble_logprob: (W, X),
-    mut rng: U,
-    a: T,
-) -> Box<dyn 'a + FnMut(&mut dyn FnMut(&Result<(W, X), McmcErr>)) -> ()>
-where
-    T: 'static + Float + NumCast + std::cmp::PartialOrd + SampleUniform + std::fmt::Display,
-    Standard: Distribution<T>,
-    U: 'static + Rng,
-    V: Clone + LinearSpace<T> + Sized,
-    for<'b> &'b V: Add<Output = V>,
-    for<'b> &'b V: Sub<Output = V>,
-    for<'b> &'b V: Mul<T, Output = V>,
-    W: 'static + Clone + IndexMut<usize, Output = V> + HasLen + Sized,
-    X: 'static + Clone + IndexMut<usize, Output = T> + HasLen + InitFromLen + Sized,
-    F: 'a + Fn(&V) -> T + Sized,
-{
-    Box::new(move |handler: &mut dyn FnMut(&Result<(W, X), McmcErr>)| {
-        let result = sample_st(&flogprob, &ensemble_logprob, &mut rng, a);
-        handler(&result);
-        if let Ok(x) = result {
-            ensemble_logprob = x;
-        }
-    })
-}
 
 pub fn sample<T, U, V, W, X, F>(
     flogprob: &F,
-    ensemble_logprob: &(W, X),
+    ensemble_logprob: &mut (W, X),
     rng: &mut U,
     a: T,
     nthread: usize,
-) -> Result<(W, X), McmcErr>
+)
 where
     T: Float + NumCast + std::cmp::PartialOrd + SampleUniform + Sync + Send + std::fmt::Display,
     Standard: Distribution<T>,
@@ -105,74 +58,40 @@ where
     X: Clone + IndexMut<usize, Output = T> + HasLen + Sync + InitFromLen + Send,
     F: Fn(&V) -> T + Send + Sync + ?Sized,
 {
-    let (ref ensemble, ref cached_logprob) = *ensemble_logprob;
+    let (ref mut ensemble, ref mut cached_logprob) = *ensemble_logprob;
     //    let cached_logprob = &ensemble_logprob.1;
-    let result_ensemble = ensemble.clone();
-    let mut result_logprob = cached_logprob.clone();
     //let pflogprob=Arc::new(&flogprob);
     let nwalkers = ensemble.len();
 
-    if nwalkers == 0 {
-        return Err(McmcErr::NWalkersIsZero);
-    }
-
-    if nwalkers % 2 != 0 {
-        return Err(McmcErr::NWalkersIsNotEven);
-    }
+    assert!(nwalkers>0);
+    assert!(nwalkers%2==0);
 
     let ndims: T = NumCast::from(ensemble[0].dimension()).unwrap();
 
     let half_nwalkers = nwalkers / 2;
-    let mut walker_group: Vec<Vec<usize>> = vec![Vec::new(), Vec::new()];
-    walker_group[0].reserve(half_nwalkers);
-    walker_group[1].reserve(half_nwalkers);
-    let mut walker_group_id: Vec<usize> = Vec::new();
-    walker_group_id.reserve(nwalkers);
-    let mut rvec: Vec<T> = Vec::new();
-    let mut jvec: Vec<usize> = Vec::new();
-    let mut zvec: Vec<T> = Vec::new();
-    rvec.reserve(nwalkers);
-    jvec.reserve(nwalkers);
-    zvec.reserve(nwalkers);
-    for i in 0..nwalkers {
-        let mut gid: usize = rng.gen_range(0, 2);
 
-        if walker_group[gid].len() == half_nwalkers {
-            gid = 1 - gid;
-        }
-        walker_group[gid].push(i);
-        walker_group_id.push(gid);
-        rvec.push(rng.gen_range(zero::<T>(), one::<T>()));
-        jvec.push(rng.gen_range(0, half_nwalkers));
-        zvec.push(draw_z(rng, a));
-    }
+    let pair_id:Vec<(usize, usize)>={
+        let mut a:Vec<usize>=(0..nwalkers).collect();
+        a.shuffle(rng);
+        a.chunks(2).map(|a| (a[0], a[1])).chain(a.chunks(2).map(|a| (a[1], a[0]))).collect()
+    };
+
+    let proposed_pt_z:Vec<_>=pair_id.iter().map(|(i1, i2)|{
+        let z=draw_z(rng, a);
+        (propose_move(&ensemble[*i1], &ensemble[*i2], z), z)
+    }).collect();
+
+    let new_logprob=Mutex::new(vec![T::zero(); nwalkers]);
 
     let atomic_k = Mutex::new(0);
-    let lp_cached = result_logprob.len() == nwalkers;
-
-    if !lp_cached {
-        //result_logprob.resize(nwalkers, T::zero());
-        result_logprob = X::init(nwalkers);
-    }
-    //let lp_cached=cached_logprob.len()!=0;
-    let result_ensemble = Mutex::new(result_ensemble);
-    let result_logprob = Mutex::new(result_logprob);
-
+    
     {
         let create_task = || {
             let atomic_k = &atomic_k;
-            let result_ensemble = &result_ensemble;
-            let result_logprob = &result_logprob;
-            let ensemble = &ensemble;
-            let cached_logprob = &cached_logprob;
-            let zvec = &zvec;
-            let walker_group = &walker_group;
-            let walker_group_id = &walker_group_id;
-            let jvec = &jvec;
+            let new_logprob=&new_logprob;
+            let proposed_pt_z=&proposed_pt_z;
             let flogprob = flogprob;
             //let rvec=Arc::clone(&rvec);
-            let rvec = &rvec;
-            //let nwalkers=nwalkers;
             move || loop {
                 let k: usize;
                 {
@@ -183,43 +102,17 @@ where
                 if k >= nwalkers {
                     break;
                 }
+                
+                let lp_y = flogprob(&proposed_pt_z[k].0);
 
-                let lp_last_y = if !lp_cached {
-                    let yy1 = flogprob(&ensemble[k]);
-                    let mut lpyy = result_logprob.lock().unwrap();
-                    lpyy[k] = yy1;
-                    yy1
-                } else {
-                    cached_logprob[k]
-                };
-
-                let i = walker_group_id[k];
-                let j = jvec[k];
-                let ni = 1 - i;
-                let z = zvec[k];
-                let r = rvec[k];
-                let new_y = scale_vec(&ensemble[k], &ensemble[walker_group[ni][j]], z);
-                let lp_y = flogprob(&new_y);
-
-                let q = ((ndims - one::<T>()) * (z.ln()) + lp_y - lp_last_y).exp();
                 {
-                    let mut yy = result_ensemble.lock().unwrap();
-                    let mut lpyy = result_logprob.lock().unwrap();
-                    if r <= q {
-                        yy[k] = new_y;
-                        lpyy[k] = lp_y;
-                    }
+                    let mut nlp=new_logprob.lock().unwrap();
+                    nlp[k]=lp_y;
                 }
             }
         };
 
         if nthread > 1 {
-            /*let mut pool = Pool::new(nthread as u32);
-            pool.scoped(|scope| {
-                for _ in 0..nthread {
-                    scope.execute(create_task());
-                }
-            });*/
             scope(|s| {
                 for _ in 0..nthread {
                     s.spawn(|_| create_task()());
@@ -231,105 +124,13 @@ where
         }
     }
 
-    let result_ensemble = result_ensemble.into_inner().unwrap();
-    let result_logprob = result_logprob.into_inner().unwrap();
-
-    Ok((result_ensemble, result_logprob))
-}
-
-pub fn sample_st<T, U, V, W, X, F>(
-    flogprob: &F,
-    ensemble_logprob: &(W, X),
-    rng: &mut U,
-    a: T,
-) -> Result<(W, X), McmcErr>
-where
-    T: Float + NumCast + std::cmp::PartialOrd + SampleUniform + std::fmt::Display,
-    Standard: Distribution<T>,
-    U: Rng,
-    V: Clone + LinearSpace<T> + Sized,
-    for<'b> &'b V: Add<Output = V>,
-    for<'b> &'b V: Sub<Output = V>,
-    for<'b> &'b V: Mul<T, Output = V>,
-    W: Clone + IndexMut<usize, Output = V> + HasLen,
-    X: Clone + IndexMut<usize, Output = T> + HasLen + InitFromLen,
-    F: Fn(&V) -> T + ?Sized,
-{
-    let (ref ensemble, ref cached_logprob) = *ensemble_logprob;
-    //    let cached_logprob = &ensemble_logprob.1;
-    let mut result_ensemble = ensemble.clone();
-    let mut result_logprob = cached_logprob.clone();
-    //let pflogprob=Arc::new(&flogprob);
-    let nwalkers = ensemble.len();
-
-    if nwalkers == 0 {
-        return Err(McmcErr::NWalkersIsZero);
-    }
-
-    if nwalkers % 2 != 0 {
-        return Err(McmcErr::NWalkersIsNotEven);
-    }
-
-    let ndims: T = NumCast::from(ensemble[0].dimension()).unwrap();
-
-    let half_nwalkers = nwalkers / 2;
-    let mut walker_group: Vec<Vec<usize>> = vec![Vec::new(), Vec::new()];
-    walker_group[0].reserve(half_nwalkers);
-    walker_group[1].reserve(half_nwalkers);
-    let mut walker_group_id: Vec<usize> = Vec::new();
-    walker_group_id.reserve(nwalkers);
-    let mut rvec: Vec<T> = Vec::new();
-    let mut jvec: Vec<usize> = Vec::new();
-    let mut zvec: Vec<T> = Vec::new();
-    rvec.reserve(nwalkers);
-    jvec.reserve(nwalkers);
-    zvec.reserve(nwalkers);
-    for i in 0..nwalkers {
-        let mut gid: usize = rng.gen_range(0, 2);
-
-        if walker_group[gid].len() == half_nwalkers {
-            gid = 1 - gid;
-        }
-        walker_group[gid].push(i);
-        walker_group_id.push(gid);
-        rvec.push(rng.gen_range(zero::<T>(), one::<T>()));
-        jvec.push(rng.gen_range(0, half_nwalkers));
-        zvec.push(draw_z(rng, a));
-    }
-
-    let lp_cached = result_logprob.len() == nwalkers;
-
-    if !lp_cached {
-        //result_logprob.resize(nwalkers, T::zero());
-        result_logprob = X::init(nwalkers);
-    }
-    //let lp_cached=cached_logprob.len()!=0;
-
-    for k in 0..nwalkers {
-        let lp_last_y = if !lp_cached {
-            let yy1 = flogprob(&ensemble[k]);
-            result_logprob[k] = yy1;
-            yy1
-        } else {
-            cached_logprob[k]
-        };
-
-        let i = walker_group_id[k];
-        let j = jvec[k];
-        let ni = 1 - i;
-        let z = zvec[k];
-        let r = rvec[k];
-        let new_y = scale_vec(&ensemble[k], &ensemble[walker_group[ni][j]], z);
-        let lp_y = flogprob(&new_y);
-
-        let q = ((ndims - one::<T>()) * (z.ln()) + lp_y - lp_last_y).exp();
-        {
-            if r <= q {
-                result_ensemble[k] = new_y;
-                result_logprob[k] = lp_y;
-            }
+    for (i, ((pt, z), (&new_lp, (i1, i2)))) in proposed_pt_z.into_iter().zip(new_logprob.into_inner().unwrap().iter().zip(pair_id.into_iter())).enumerate(){
+        let lp_last_y=cached_logprob[i1];
+        let q = ((ndims - one::<T>()) * (z.ln()) + new_lp - lp_last_y).exp();
+        if rng.gen_range(T::zero(), T::one()) < q{
+            ensemble[i1]=pt;
+            cached_logprob[i1]=new_lp;
         }
     }
-
-    Ok((result_ensemble, result_logprob))
 }
+
