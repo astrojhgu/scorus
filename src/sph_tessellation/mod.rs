5013b58018a35ead75164d1865033aadd6940fed
GU Junhua <jhgu@nao.cas.cn>
2022-05-20 23:18:06 +0800

    modified:   src/sph_tessellation/mod.rs

--- src/sph_tessellation/mod.rs
+++ src/sph_tessellation/mod.rs
@@ -124,7 +124,6 @@ where
     }
 
     pub fn regulate_norm(&mut self) ->Vec<Vec3d<T>>{
-        println!("**********************************");
         self.faces.iter_mut().map(|v| {
             let v0 = self.vertices[v[0]];
             let v1 = self.vertices[v[1]];
